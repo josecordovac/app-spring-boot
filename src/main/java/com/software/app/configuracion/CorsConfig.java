b723ef7629ec886dfc46d023e32ddb9d42f73987
Admin <admin@jaccxsoftware.pe>
2025-06-13 01:38:13 -0500

    update cors

--- src/main/java/com/software/app/configuracion/CorsConfig.java
+++ src/main/java/com/software/app/configuracion/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**") // Habilitar CORS para rutas API
-                        .allowedOrigins("https://d3jo6hjdfzjjhe.cloudfront.net") // Origen permitido
+                        .allowedOrigins("http://d30m6h6igzbs1e.cloudfront.net") // Origen permitido
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Métodos permitidos
                         .allowedHeaders("*") // Permitir todos los headers
                         .allowCredentials(true); // Permitir credenciales si es necesario
